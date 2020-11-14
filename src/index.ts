bf67e4de85efac00ba370c767c589ea8142f7130
Stevan Goode <steve@madhouseminers.com>
2020-11-14 10:58:00 +0000

    Removed hard coded address

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { AddressInfo } from "net";
 import app from "./express";
 
 const server = http.createServer(app);
-server.listen({ port: process.env.PORT || 8080, host: "127.0.0.1" }, () => {
+server.listen(process.env.PORT || 8080, () => {
   const address = server.address() as AddressInfo;
   console.log(`👨‍🚀 Listening on http://${address.address}:${address.port}`);
 });
